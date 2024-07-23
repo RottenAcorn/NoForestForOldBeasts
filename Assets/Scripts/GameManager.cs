161c43903f97d0b697b83cb58ea149ea3b4714f4
Szymon Zasada <szymon.zasadaa@gmail.com>
2024-07-23 16:54:17 +0200

    ✨feat: added player movement action + entity handler + pooling manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GameManager : NetworkBehaviour
+{
+    #region Singleton
+    public static GameManager Instance;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    #endregion
+    
+    public IEnumerable<PlayableEntity> PlayableEntities;
+
+    public System.Action OnUpdate;
+    public void Start()
+    {
+        //starting immediately as host for testing
+        SteamManager.Instance.HostLobby();
+
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnected;
+    }
+
+    private void OnClientConnected(ulong clientId)
+    {
+        Debug.Log("Client Connected: " + clientId);
+
+        PlayableEntities.Append(NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.GetComponent<PlayableEntity>());
+    }
+
+    private void FixedUpdate()
+    {
+        OnUpdate?.Invoke();
+    }
+
+    private void OnClientDisconnected(ulong clientId)
+    {
+        Debug.Log("Client Disconnected: " + clientId);
+
+        //remove from IEnumerable
+        PlayableEntities = PlayableEntities.Where(x => x != NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.GetComponent<PlayableEntity>());
+    }
+
+}
---
+++ Assets/Scripts/Models/ExecuteActionsSample.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+
+public class ServerJob : IJob
+{
+
+    public void Execute()
+    {
+
+    }
+}
